19fc0a98618c045ae62ed26107f92ae0adb9adfb
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2026-08-24 11:53:44 +0530

    V1 of the ROll Dice app built using simple javascript knowledge

---
+++ RollTheDice05/src/App.tsx
@@ -0,0 +1,72 @@
+import { Image,  ImageSourcePropType, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { JSX,  PropsWithChildren, useState } from 'react'
+import DiceOne from '../assets/One.png'
+import DiceTwo from '../assets/Two.png'
+import DiceThree from '../assets/Three.png'
+import DiceFour from '../assets/Four.png'
+import DiceFive from '../assets/Five.png'
+import DiceSix from '../assets/Six.png'
+
+type DiceProps = PropsWithChildren<{
+  imageUrl: ImageSourcePropType
+}>
+
+const Dice = ({imageUrl}: DiceProps): JSX.Element => {
+  return (
+    <View>
+      <Image style={styles.image} source={imageUrl} />
+    </View>
+  )
+}
+
+export default function App() {
+  // current image is a number from 1 to 6
+  const [currentImage, setCurrentImage] = useState(Number(0))
+  const images=[require("../assets/One.png"), require("../assets/Two.png"), require("../assets/Three.png"), require("../assets/Four.png"), require("../assets/Five.png"), require("../assets/Six.png")]
+  
+  function handlePress() {
+    const randomNumber = Math.floor(Math.random() * 6)
+    setCurrentImage(randomNumber)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image source={images[currentImage]} />
+      <TouchableOpacity onPress={handlePress}>
+        <View style={styles.button}>
+          <Text style={styles.btnText}>Roll Dice</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#854ffa',
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    marginTop: 25,
+    borderRadius: 25,
+    width: 150,
+    alignItems: 'center',
+    elevation: 5,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+  },
+  btnText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  image: {
+    width: 200,
+    height: 200
+  }
+})

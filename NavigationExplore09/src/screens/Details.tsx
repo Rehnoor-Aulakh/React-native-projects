4be4c8be56bd7c0ec4322955d47c35158bee2af3
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2026-08-26 11:51:08 +0530

    Navigation Setup done with Home screen and Details screen using Native Stack of React Navigation

---
+++ NavigationExplore09/src/screens/Details.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Details() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.smallText}>Details</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    smallText: {
+        color: 'black',
+        
+    }
+})
